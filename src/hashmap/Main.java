a9cd13d554f0518689fbf071ff455122e5241109
sirhovnanyan <hovnanyansiranush@gmail.com>
2021-10-21 23:56:28 +0400

    Sixth commit

--- src/hashmap/Main.java
+++ src/hashmap/Main.java
@@ -3,18 +3,18 @@ package hashmap;
 public class Main {
     public static void main(String[] args) {
         MyHashMap<Integer, String> myMap = new MyHashMap<>();
-        System.out.println(myMap.isEmpty());
+           System.out.println(myMap.isEmpty());
         myMap.put(1, "Hi");
         myMap.put(2, "Hello");
         myMap.put(9, "Good morning");
         myMap.put(12, "Good afternoon");
         myMap.put(24, "Good night");
-        System.out.println(myMap.containsKey(4));
-        System.out.println(myMap.get(12));
+            System.out.println(myMap.containsKey(4));
+            System.out.println(myMap.get(12));
         myMap.remove(1);
-        System.out.println(myMap.size());
-        System.out.println(myMap.isEmpty());
-        System.out.print("MyMap is: ");
+            System.out.println(myMap.size());
+            System.out.println(myMap.isEmpty());
+             System.out.print("MyMap is: ");
         myMap.display();
     }
 }
